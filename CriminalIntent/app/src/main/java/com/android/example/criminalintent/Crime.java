339f98299194bc038ef54dcd627dc4e094f43f14
sergeykamyshov <kamishovsergey@gmail.com>
2017-01-19 22:50:49 +0300

    Created CriminalIntent app from chapter 7

---
+++ CriminalIntent/app/src/main/java/com/android/example/criminalintent/Crime.java
@@ -0,0 +1,25 @@
+package com.android.example.criminalintent;
+
+import java.util.UUID;
+
+public class Crime {
+
+    UUID mId;
+    String mTitle;
+
+    public Crime() {
+        mId = UUID.randomUUID();
+    }
+
+    public UUID getmId() {
+        return mId;
+    }
+
+    public String getmTitle() {
+        return mTitle;
+    }
+
+    public void setmTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+}
