5b2b6266674928bbdc7264b3d3eed56084c02285
sergeykamyshov <kamishovsergey@gmail.com>
2017-01-21 17:57:25 +0300

    Added several views on fragment and created land orientation

--- CriminalIntent/app/src/main/java/com/android/example/criminalintent/Crime.java
+++ CriminalIntent/app/src/main/java/com/android/example/criminalintent/Crime.java
@@ -1,25 +1,45 @@
 package com.android.example.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class Crime {
 
-    UUID mId;
-    String mTitle;
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
 
     public Crime() {
         mId = UUID.randomUUID();
+        mDate = new Date();
     }
 
-    public UUID getmId() {
+    public UUID getId() {
         return mId;
     }
 
-    public String getmTitle() {
+    public String getTitle() {
         return mTitle;
     }
 
-    public void setmTitle(String mTitle) {
-        this.mTitle = mTitle;
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
     }
 }
--- CriminalIntent/app/src/main/java/com/android/example/criminalintent/CrimeFragment.java
+++ CriminalIntent/app/src/main/java/com/android/example/criminalintent/CrimeFragment.java
@@ -9,12 +9,21 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
+
+import java.text.DateFormat;
+
+//import android.text.format.DateFormat;
 
 public class CrimeFragment extends Fragment {
 
     private Crime mCrime;
     private TextView mTitleField;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckBox;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -37,12 +46,25 @@ public class CrimeFragment extends Fragment {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                mCrime.setmTitle(s.toString());
+                mCrime.setTitle(s.toString());
             }
 
             @Override
             public void afterTextChanged(Editable s) {
 
+            }
+        });
+
+        mDateButton = (Button) v.findViewById(R.id.crime_date);
+        DateFormat dateFormat = DateFormat.getDateInstance();
+        mDateButton.setText(dateFormat.format(mCrime.getDate()));
+        mDateButton.setEnabled(false);
+
+        mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mCrime.setSolved(isChecked);
             }
         });
 
