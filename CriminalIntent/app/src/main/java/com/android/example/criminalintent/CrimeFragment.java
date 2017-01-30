ab30bde3d99db7f09473d3f274322fb19842a1cb
sergeykamyshov <kamishovsergey@gmail.com>
2017-01-30 12:29:19 +0300

    Added second activity with actual info for selected item

--- CriminalIntent/app/src/main/java/com/android/example/criminalintent/CrimeFragment.java
+++ CriminalIntent/app/src/main/java/com/android/example/criminalintent/CrimeFragment.java
@@ -15,10 +15,11 @@ import android.widget.CompoundButton;
 import android.widget.TextView;
 
 import java.text.DateFormat;
-
-//import android.text.format.DateFormat;
+import java.util.UUID;
 
 public class CrimeFragment extends Fragment {
+
+    private static final String ARG_CRIME_ID = "crime_id";
 
     private Crime mCrime;
     private TextView mTitleField;
@@ -28,8 +29,8 @@ public class CrimeFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mCrime = new Crime();
-        Log.i("CrimeFragment", "onCreate()");
+        UUID crimeId = (UUID) getArguments().getSerializable(ARG_CRIME_ID);
+        mCrime = CrimeLab.get(getActivity()).getCrime(crimeId);
     }
 
     @Nullable
@@ -38,6 +39,7 @@ public class CrimeFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_crime, container, false);
 
         mTitleField = (TextView) v.findViewById(R.id.crime_title);
+        mTitleField.setText(mCrime.getTitle());
         mTitleField.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -61,6 +63,7 @@ public class CrimeFragment extends Fragment {
         mDateButton.setEnabled(false);
 
         mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setChecked(mCrime.isSolved());
         mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -70,5 +73,15 @@ public class CrimeFragment extends Fragment {
 
         Log.i("CrimeFragment", "onCreateView()");
         return v;
+    }
+
+    public static Fragment newFragment(UUID crimeId) {
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_CRIME_ID, crimeId);
+
+        CrimeFragment fragment = new CrimeFragment();
+        fragment.setArguments(args);
+
+        return fragment;
     }
 }
