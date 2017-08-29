32875a1ec2e25c96b6b6530b963543316d0ede2e
kamyshov.sergey <kamyshov.sergey@otr.ru>
2017-08-29 19:00:17 +0300

    Added method that creates url with parameters

--- PhotoGallery/app/src/main/java/com/kamyshovcorp/photogallery/FlickrFetchr.java
+++ PhotoGallery/app/src/main/java/com/kamyshovcorp/photogallery/FlickrFetchr.java
@@ -1,5 +1,8 @@
 package com.kamyshovcorp.photogallery;
 
+import android.net.Uri;
+import android.util.Log;
+
 import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -10,43 +13,58 @@ import java.net.URL;
 
 public class FlickrFetchr {
 
+    public static final String TAG = "FlickrFetchr";
+    public static final String API_KEY = "14013a8302dbb9fe20d2662c538f09eb";
+
     public byte[] getUrlBytes(String urlSpec) throws IOException {
         URL url = new URL(urlSpec);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
         try {
-//            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-//
-//            StringBuilder builder = new StringBuilder();
-//            String line = "";
-//            while ((line = bufferedReader.readLine()) != null) {
-//                builder.append(line);
-//            }
-
-
-            ByteArrayOutputStream out = new ByteArrayOutputStream();
-//            connection.setRequestProperty("Accept-Encoding", "identity");
-//            connection.setRequestProperty("Accept-Charset", "UTF-8");
-            InputStream in = connection.getInputStream();
-
             if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
                 throw new IOException(connection.getResponseMessage() + ": with " + urlSpec);
             }
 
-            int bytesRead = 0;
-            byte[] buffer = new byte[1024];
-            while ((bytesRead = in.read()) > 0) {
-                out.write(buffer, 0, bytesRead);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuilder builder = new StringBuilder();
+            String line = "";
+            while ((line = bufferedReader.readLine()) != null) {
+                builder.append(line);
             }
-            out.close();
-            return out.toByteArray();
+            return builder.toString().getBytes();
+
+//            ByteArrayOutputStream out = new ByteArrayOutputStream();
+//            InputStream in = connection.getInputStream();
+//            int bytesRead = 0;
+//            byte[] buffer = new byte[1024];
+//            while ((bytesRead = in.read()) > 0) {
+//                out.write(buffer, 0, bytesRead);
+//            }
+//            out.close();
+//            return out.toByteArray();
         } finally {
             connection.disconnect();
         }
     }
 
     public String getUrlString(String urlSpec) throws IOException {
-//        return new String(getUrlBytes(urlSpec), "UTF-8");
         return new String(getUrlBytes(urlSpec));
+    }
+
+    public void fetchItems() {
+        try {
+            String url = Uri.parse("https://api.flickr.com/services/rest/")
+                    .buildUpon()
+                    .appendQueryParameter("method", "flickr.photos.getRecent")
+                    .appendQueryParameter("api_key", API_KEY)
+                    .appendQueryParameter("format", "json")
+                    .appendQueryParameter("nojsoncallback", "1")
+                    .appendQueryParameter("extras", "url_s")
+                    .build().toString();
+            String jsonString = getUrlString(url);
+            Log.i(TAG, "Received from JSON: " + jsonString);
+        } catch (IOException e) {
+            Log.e(TAG, "Failed to fetch items", e);
+        }
     }
 }
